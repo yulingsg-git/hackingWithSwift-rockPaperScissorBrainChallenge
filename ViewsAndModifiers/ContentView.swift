20b276b18c6401821a4b2d6507d6b95a42182cb8
Chen Yuling <yuling.sg@gmail.com>
2026-05-18 19:20:48 +0900

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Chen Yuling on 2026/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Chen Yuling on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
